06504a22042dcf13c2725db73c009c3662f826ab
patrick <patrick@adaptilab.com>
2020-08-07 00:07:04 -0700

    Change transformer_main.py to use functions from transformer_utils

--- transformer_main.py
+++ transformer_main.py
@@ -133,100 +133,17 @@ for epoch in range(EPOCHS):
 
   print ('Time taken for 1 epoch: {} secs\n'.format(time.time() - start))
 
-def evaluate(inp_sentence):
-  start_token = [tokenizer_pt.vocab_size]
-  end_token = [tokenizer_pt.vocab_size + 1]
-  
-  # inp sentence is portuguese, hence adding the start and end token
-  inp_sentence = start_token + tokenizer_pt.encode(inp_sentence) + end_token
-  encoder_input = tf.expand_dims(inp_sentence, 0)
-  
-  # as the target is english, the first word to the transformer should be the
-  # english start token.
-  decoder_input = [tokenizer_en.vocab_size]
-  output = tf.expand_dims(decoder_input, 0)
-    
-  for i in range(MAX_LENGTH):
-    enc_padding_mask, combined_mask, dec_padding_mask = create_masks(
-        encoder_input, output)
-  
-    # predictions.shape == (batch_size, seq_len, vocab_size)
-    predictions, attention_weights = transformer(encoder_input, 
-                                                 output,
-                                                 False,
-                                                 enc_padding_mask,
-                                                 combined_mask,
-                                                 dec_padding_mask)
-    
-    # select the last word from the seq_len dimension
-    predictions = predictions[: ,-1:, :]  # (batch_size, 1, vocab_size)
+from translate_utils import translate
 
-    predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
-    
-    # return the result if the predicted_id is equal to the end token
-    if predicted_id == tokenizer_en.vocab_size+1:
-      return tf.squeeze(output, axis=0), attention_weights
-    
-    # concatentate the predicted_id to the output which is given to the decoder
-    # as its input.
-    output = tf.concat([output, predicted_id], axis=-1)
-
-  return tf.squeeze(output, axis=0), attention_weights
-
-def plot_attention_weights(attention, sentence, result, layer):
-  fig = plt.figure(figsize=(16, 8))
-  
-  sentence = tokenizer_pt.encode(sentence)
-  
-  attention = tf.squeeze(attention[layer], axis=0)
-  
-  for head in range(attention.shape[0]):
-    ax = fig.add_subplot(2, 4, head+1)
-    
-    # plot the attention weights
-    ax.matshow(attention[head][:-1, :], cmap='viridis')
-
-    fontdict = {'fontsize': 10}
-    
-    ax.set_xticks(range(len(sentence)+2))
-    ax.set_yticks(range(len(result)))
-    
-    ax.set_ylim(len(result)-1.5, -0.5)
-        
-    ax.set_xticklabels(
-        ['<start>']+[tokenizer_pt.decode([i]) for i in sentence]+['<end>'], 
-        fontdict=fontdict, rotation=90)
-    
-    ax.set_yticklabels([tokenizer_en.decode([i]) for i in result 
-                        if i < tokenizer_en.vocab_size], 
-                       fontdict=fontdict)
-    
-    ax.set_xlabel('Head {}'.format(head+1))
-  
-  plt.tight_layout()
-  plt.show()
-
-def translate(sentence, plot=''):
-  result, attention_weights = evaluate(sentence)
-  
-  predicted_sentence = tokenizer_en.decode([i for i in result 
-                                            if i < tokenizer_en.vocab_size])  
-
-  print('Input: {}'.format(sentence))
-  print('Predicted translation: {}'.format(predicted_sentence))
-  
-  if plot:
-    plot_attention_weights(attention_weights, sentence, result, plot)
-
-translate("este é um problema que temos que resolver.")
+translate("este é um problema que temos que resolver.", tokenizer_en, tokenizer_pt, transformer)
 print("Real translation: this is a problem we have to solve .")
 
-translate("os meus vizinhos ouviram sobre esta ideia.")
+translate("os meus vizinhos ouviram sobre esta ideia.", tokenizer_en, tokenizer_pt, transformer)
 print("Real translation: and my neighboring homes heard about this idea .")
 
-translate("vou então muito rapidamente partilhar convosco algumas histórias de algumas coisas mágicas que aconteceram.")
+translate("vou então muito rapidamente partilhar convosco algumas histórias de algumas coisas mágicas que aconteceram.", tokenizer_en, tokenizer_pt, transformer)
 print("Real translation: so i 'll just share with you some stories very quickly of some magical things that have happened .")
 
-translate("este é o primeiro livro que eu fiz.", plot='decoder_layer4_block2')
+translate("este é o primeiro livro que eu fiz.", tokenizer_en, tokenizer_pt, transformer, plot='decoder_layer4_block2')
 print("Real translation: this is the first book i've ever done.")
 
